2642f51550210a962811b3496a2a48a16958ee15
shtcheng <240469633@qq.com>
2017-05-08 17:34:50 +0800

    get appkey

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ App({
 
     //微信登陆
     wx.login({
-      success: function(res){  
+      success: function(res){
         //用户信息
         wx.getUserInfo({
           success: function (res) {
@@ -22,9 +22,35 @@ App({
           method: 'GET',
           success: function(res){
             that.globalData.openid = res.data.openid;
+            console.log("openid");
             console.log(that.globalData.openid)
           }
         });
+
+        // pAppKey
+        var strUrl2 = that.globalData.rootUrl+'/appkey?appid='+that.globalData.pAppId;
+
+        wx.request({
+          url: strUrl2,
+          data: {},
+          method: 'GET',
+          success: function(res){
+            console.log("get appkey success");
+            var d = JSON.parse(res.data.data);
+            that.globalData.pAppKey = d.key;
+            console.log(that.globalData.pAppKey);
+          },
+
+          fail:function(res) {
+            console.log("appkey fail");
+
+          },
+          complete:function(res) {
+            console.log("appkey complete");
+
+          }
+        });
+
       }
     });
   },
@@ -32,7 +58,7 @@ App({
   globalData:{
     appid:"wxb1e9f107aff08a66",
     secret:"0ccd7143b59b99ae3fa50aecab312763",
-    rootUrl:"www.iland.cc:8000",//服务器url
+    rootUrl:"https://iland.cc",//服务器url
     userInfo:null,//用户微信信息 
     openid:"",
 
@@ -44,6 +70,6 @@ App({
     phone:"",//手机号
     pAppId:"cpp.7f0ef3584bd58f14891ca3646378",//用户平台appid
     pAppKey:"",//用户平台app key 
-    pUserInfo:null,//用户平台信息   
+    pUserInfo:null,//用户平台信息
   }
 })
